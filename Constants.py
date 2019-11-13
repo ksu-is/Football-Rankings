0554a935be6ae61456604eb2355a5799b86de342
JacobBurdetteKSU <54455722+JacobBurdetteKSU@users.noreply.github.com>
2019-11-13 15:32:22 -0500

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,19 @@
+# Constants
+TEAM_LIST    = 'util/csvs/teams.txt'
+SCORE_URL    = 'http://prwolfe.bol.ucla.edu/cfootball/scores.htm'
+SCHEDULE_URL = 'http://prwolfe.bol.ucla.edu/cfootball/schedules.htm'
+
+YEAR      = 2019
+# YEAR      = 2018
+WEEK      = 12
+K_VALUE   = 18
+
+SEED_DATE = '24-Aug-19'
+
+START_DATE = '13-Nov-19'
+END_DATE   = '20-Nov-19'
+
+# Betting Info
+ODDS_URL = 'https://services.bovada.lv/services/sports/event/coupon/events/A/description/football/college-football?marketFilterId=def&preMatchOnly=true&lang=en'
+SEED_MONEY = 100
+BET_SIZE = SEED_MONEY / 20
